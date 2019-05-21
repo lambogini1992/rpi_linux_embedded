337a8872775d601a1df9958e4781222221148a11
OEM Configuration (temporary user) <phamtuananhvnvt1992@gmail.com>
2019-05-21 21:22:51 +0700

    Remove invalid project

--- mq_client/Debug/mq_lib/msg_queue.d
+++
@@ -1,3 +0,0 @@
-mq_lib/msg_queue.o: ../mq_lib/msg_queue.c ../mq_lib/./msg_queue.h
-
-../mq_lib/./msg_queue.h:
--- mq_server/Debug/main.d
+++
@@ -1,4 +0,0 @@
-main.o: ../main.c \
- /home/anhpt26/eclipse-workspace/mq_server/mq_lib/msg_queue.h
-
-/home/anhpt26/eclipse-workspace/mq_server/mq_lib/msg_queue.h:
