af9912663c6886391a32b94f564e1b86e16e908e
anhptvt1992 <phamtuananhvnvt1992@gmail.com>
2019-07-17 20:53:36 -0700

    Modify gpio_dev.c

--- device_tree_platform_gpio/gpio_dev.c
+++ device_tree_platform_gpio/gpio_dev.c
@@ -5,3 +5,14 @@
 /*using for platform device*/
 #include <linux/of.h>
 #include <linux/platform_device.h>
+
+static int gpio_driver_probe(struct platform_device *pdev);
+static int gpio_driver_remove(struct platform_device *pdev);
+
+static const struct of_device_id example_gpio_of_match[] = {
+	{ .compatible = "test,test-rpi2b-gpio",  },
+	{ /* sentinel */ },
+};
+
+MODULE_DEVICE_TABLE(of, flexcan_of_match);
+
