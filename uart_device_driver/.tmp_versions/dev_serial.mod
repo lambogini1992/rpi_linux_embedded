5905d119a34cacf4696fc6e8a923d90929dce965
anhptvt1992 <lambogini1992@gmail.com>
2019-10-03 02:02:40 -0700

    Update new source code

--- uart_device_driver/.tmp_versions/dev_serial.mod
+++
@@ -1,3 +0,0 @@
-/home/anhpt26/git_project/rpi_linux_embedded/uart_device_driver/dev_serial.ko
-/home/anhpt26/git_project/rpi_linux_embedded/uart_device_driver/dev_serial.o
-
--- uart_device_driver/dev_serial.c
+++ uart_device_driver/dev_serial.c
@@ -1,4 +1,4 @@
-
+p
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -16,9 +16,48 @@
 #include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/platform_device.h>
+#include <linux/serial_core.h>
 
 
 #define DRIVER_NAME "DEV_SERIAL"
+#define DEV_NAME    "tty_dev_s"
+
+/*THIS IS DATA BUFFER IF BIT DLAB = 0*/
+#define AUX_MU_IO_REG								0
+
+/*ENABLE INTERRUPT IF BIT DLAB = 0*/
+/*SET BAUDRATE IF BIT DLAB = 1*/
+#define AUX_MU_IIR_REG							1
+
+/*THIS REGISTER IS SHOW THE INTERRUPT STATUS*/
+/*CAN USING FOR CLEAR FIFO REVC AND TRANS BUFFER*/
+#define AUX_MU_IER_REG							2
+
+/*THIS REGISTER IS USED FOR CONTROL LINE DATA FORMAT
+**GIVES ACCESS BAUDRATE REGISTER*/
+#define AUX_MU_LCR_REG 							3
+
+/*register controls the 'modem' signals*/
+#define AUX_MU_MCR_REG 							4
+
+/*register shows the data status*/
+#define AUX_MU_LSR_REG 							5
+
+/*register shows the 'modem' status*/
+#define AUX_MU_MSR_REG							6
+
+#define AUX_MU_SCRATCH							7
+
+/*provides access to some extra useful and nice features not
+**found on a normal 16550 UART*/
+#define AUX_MU_CNTL_REG 						8
+
+/*provides a lot of useful information about the internal status*/
+#define AUX_MU_STAT_REG 						9
+
+/*USING FOR SETTING BAUDRATE of device*/
+#define AUX_MU_BAUD									10
+
 
 
 
@@ -28,6 +67,12 @@ static volatile uint32_t *gpio_base = NULL;
 struct resource *res;
 uint8_t set_level;
 
+typedef struct _bcm_uart_
+{
+	struct uart_port port;
+
+}
+
 
 struct _exam_dev_
 {
@@ -36,11 +81,6 @@ struct _exam_dev_
 	struct class *dev_cls;
 	struct device *dev;
 }exam_dev;
-
-/*Function declared for gpio*/
-static void set_direction(uint8_t gpio_pin);
-static void set_pin_level(uint8_t gpio_pin, uint8_t level);
-static int read_level_gpio(uint16_t pin);
 
 /*Register character device*/
 static int dev_open(struct inode *, struct file *);
@@ -58,89 +98,19 @@ static struct file_operations fops = {
 	.write = dev_write,
 };
 
-/*FUNCTION GPIO HARDWARE*/
-static void set_direction(uint8_t gpio_pin)
+
+static inline void uart_write_reg(struct uart_port *port, u32 reg, u32 val)
 {
-	// int major_pin;
-	// int minor_pin;
-	//
-	// major_pin = gpio_pin/10;
-	// minor_pin = gpio_pin - (major_pin*10);
-	//
-	// switch(major_pin)
-	// {
-	// 	case 0:
-	// 		gpio_base[GPFSEL0] |= 1 << minor_pin;
-	// 		break;
-	//
-	// 	case 1:
-	// 		gpio_base[GPFSEL1] |= 1 << minor_pin;
-	// 		break;
-	//
-	// 	case 2:
-	// 		gpio_base[GPFSEL2] |= 1 << minor_pin;
-	// 		break;
-	//
-	// 	case 3:
-	// 		gpio_base[GPFSEL0] |= 1 << minor_pin;
-	// 		break;
-	// }
+	__raw_write(val, port->membase + reg);
 }
 
-static void set_pin_level(uint8_t gpio_pin, uint8_t level)
+static inline u32 uart_read_reg(struct uart_port *port, u32 reg)
 {
-	// uint8_t pin;
-	//
-	// pin = gpio_pin;
-	// switch(level)
-	// {
-	// 	case 0:
-	// 		if(pin <= 31)
-	// 		{
-	// 			gpio_base[GPCLR0] |= 1 << pin;
-	// 		}
-	// 		else
-	// 		{
-	// 			pin = pin - 31;
-	// 			gpio_base[GPCLR1] |= 1 << pin;
-	// 		}
-	// 		break;
-	//
-	// 	case 1:
-	// 		if(pin <= 31)
-	// 		{
-	// 			gpio_base[GPSET0] |= 1 << pin;
-	// 		}
-	// 		else
-	// 		{
-	// 			pin = pin - 31;
-	// 			gpio_base[GPSET1] |= 1 << pin;
-	// 		}
-	// 		break;
-	//
-	// 	default:
-	// 		printk("Wrong level for gpio pin\n");
-	// 		break;
-	// }
+	 return __raw_read(port->membase + reg);
 }
 
-static int read_level_gpio(uint16_t pin)
-{
-	// int level_pin;
-	//
-	// if(pin <= 31)
-	// {
-	// 	level_pin = gpio_base[GPLEV0] & (1 << pin);
-	// 	level_pin = level_pin >> pin;
-	// }
-	// else
-	// {
-	// 	pin = pin - 31;
-	// 	level_pin = gpio_base[GPLEV1] & (1 << pin);
-	// 	level_pin = level_pin >> pin;
-	// }
-	return (int)0;
-}
+static inline
+
 /*FUNCTION DEVICE FILE*/
 static int dev_open(struct inode *inode, struct file *fp)
 {
@@ -163,42 +133,6 @@ static ssize_t dev_read(struct file *fp, char __user *ubuf, size_t size_of, loff
 
 static ssize_t dev_write(struct file *fp, const char __user *buf, size_t size_of, loff_t *offset)
 {
-	char *kernel_buff = NULL;
-	char *buff_cmd = NULL;
-
-	kernel_buff = kzalloc(size_of, GFP_KERNEL);
-	buff_cmd 	= kzalloc(size_of, GFP_KERNEL);
-
-	if(NULL == kernel_buff)
-	{
-		printk("fail to allocate kernel_buff\n");
-		return 0;
-	}
-
-	if(NULL == buff_cmd)
-	{
-		printk("fail to allocate buff_cmd\n");
-		return 0;
-	}
-
-	if(copy_from_user(kernel_buff, buf, size_of))
-	{
-		printk("fail to get data from user buffer\n");
-		return -EFAULT;
-	}
-
-	snprintf(buff_cmd, size_of, "%s", kernel_buff);
-
-	if(strcmp(buff_cmd, "1") == 0)
-	{
-		set_pin_level(18, 1);
-	}
-	else if(strcmp(buff_cmd, "0") == 0)
-	{
-		set_pin_level(18, 0);
-	}
-	kfree(kernel_buff);
-	kfree(buff_cmd);
 	return size_of;
 }
 
